b59d76e926a6499a1f55ccd6d9d5a04eae82849e
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2023-07-02 15:00:59 -0400

    Connecting frontend to backend
    Connecting backend to frontend

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {Link} from 'react-router-dom'
+import { register, reset } from '../features/auth/authSlice'
 
 function Register() {
-  return (
+    const dispatch = useDispatch()
+    const [registerData, setRegisterData] = useState({
+        'firstName': '', 
+        'lastName': '',
+        'email' : '', 
+        'password': '',
+        'phone': '', 
+    })
+    const {firstName, lastName, email, password, phone} = registerData
+    const onChange = (e) => {
+        setRegisterData((prevState) => (
+            {
+                ...prevState, 
+                [e.target.name]: e.target.value
+            }
+        ))
+    }
+
+    const onSubmit = (e) =>{
+        // dispatch(reset)
+        e.preventDefault()
+        console.log(registerData)
+        dispatch(register(registerData))
+    }
+  
+  
+    return (
     <div>
         <div className='register--left'>
             <h3>Create Account</h3>
             <p>Create your Proteus account to start earing points and rewards today</p>
-            <form action="">
-                <label htmlFor="">First Name</label>
-                <input type="text" />
-                <label htmlFor="">Last Name</label>
-                <input type="text" />
-                <label htmlFor="">Email</label>
-                <input type="email" />
-                <label htmlFor="">Password</label>
-                <input type="password" />
-                <label htmlFor="">Phone</label>
-                <input type="numbers" />
+            <form onSubmit={onSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" name='firstName' id='firstName' value={firstName} onChange={onChange}/>
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" name='lastName' id='lastName' value={lastName} onChange={onChange}/>
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' id='email' value={email} onChange={onChange}/>
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' id='password' value={password} onChange={onChange} />
+                <label htmlFor="phone">Phone</label>
+                <input type="text" id='phone' name='phone' value={phone} onChange={onChange} />
             </form>
             <p>By clicking 'Create Account', I acknowledge and agree to Proteus' Privacy Policy, Condition of Use, and the Proteus Terms and Conditions</p>
-            <button>Create Account</button>
+            <button onClick={onSubmit}>Create Account</button>
             <p>or</p>
             <Link to='/login'><button>Sign in</button></Link>
         </div>
---
+++ src/features/auth/authService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API = axios.create({baseURL: 'http://localhost8080/'})
+
+const register = async (userData) => {
+    const response = await API.post('api/auth/register', userData)
+    if(response.data) {
+        localStorage.setItem('auth', JSON.stringify(response.data))
+    }
+    return response.data
+}
+
+
+const authServices = {
+    register
+}
+
+export default authServices
